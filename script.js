38916deb686cad7805d56e0ee0db0ac872f8626f
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-02-23 19:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let student={
+	name:"Neha",
+	// age:"25",
+	// city:"patna"
+}
+function getKeys(obj){
+	let ans=[];
+
+	return Object.keys(student)
+	
+	// return ans;
+}
