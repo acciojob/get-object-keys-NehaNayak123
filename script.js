2773550a9ab8d1f9a2a46d66767c2c14376d7e0a
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 20:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 //your JS code here. If required.
-let student={
-	name:"Neha",
-	getKeys:(obj)=>{
-		return Object.keys(obj);
-	}
-}
-// console.log(getKeys(student))
+let student = {
+  name: "Neha",
+  top: "67",
+  getKeys: function () {
+    return Object.keys(this); 
+  }
+};
+console.log(student.getKeys()); // Output: ["name", "top", "getKeys"]
 
-console.log(student.getKeys(student))
+
+
+// let student={
+// 	name:"Neha",
+// 	getKeys:(obj)=>{
+// 		return Object.keys(obj);
+// 	}
+// }
+// // console.log(getKeys(student))
+
+// console.log(student.getKeys(student))
 // function getKeys(obj){
 // 	return Object.keys(obj);
 // }
