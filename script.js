96f5ba6e9df391c16f95e102f0ca3594b08b5568
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 20:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-//your JS code here. If required.
-let student = {
-  name: "Neha",
-  top: "67",
-  getKeys: function () {
-    return Object.keys(this); 
-  }
+// Create the student object
+const student = {
+  name: "John"
 };
-console.log(student.getKeys()); // Output: ["name", "top", "getKeys"]
 
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
 
-
-// let student={
-// 	name:"Neha",
-// 	getKeys:(obj)=>{
-// 		return Object.keys(obj);
-// 	}
-// }
-// // console.log(getKeys(student))
-
-// console.log(student.getKeys(student))
-// function getKeys(obj){
-// 	return Object.keys(obj);
-// }
-
-// console.log(getKeys(student))
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
