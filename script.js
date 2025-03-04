afd5d066ed1884aa698f673788aa61298366c55e
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-04 16:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// Create the student object
+
 const student = {
   name: "John"
 };
-
-// Function to get all keys from the object
+// TypeError: myObj.getKeys is not a function
 function getKeys(obj) {
-  return Object.keys(obj);
+  // return Object.keys(obj);
 }
 
-// Testing the function
-console.log(getKeys(student)); // Output: ["name"]
+Object.prototype.getKeys=function () {
+	return Object.keys(this);
+}
+
+// console.log(getKeys(student)); 
